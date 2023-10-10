528ac1be1b9be437c13728f762ddc246237473fc
Malik Saleem <maliksaleem32596@gmail.com>
2023-10-10 19:40:46 +0530

    first commit

---
+++ src/Text.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./forms.css";
+
+function Text() {
+  return (
+    <div className='textform'>
+        <h1 style={{fontSize:'60px'}}>EMPLOYEE FEEDBACK FORM</h1>
+        </div>
+  )
+}
+
+export default Text
